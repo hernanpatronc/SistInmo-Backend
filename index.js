68c88323c0e1ec3538c075e639c83ad107db8194
Hernan Patron Costas <hernanpatroncostas@gmail.com>
2017-08-03 19:45:06 -0300

    Estadisticas y usuarios, login

--- index.js
+++ index.js
@@ -21,42 +21,104 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
-  database : 'pcya'
+  database : 'pcya',
+  multipleStatements: true,
 });
+
 connection.connect(function(err){
 	if(err) console.log(err);
-	else console.log("Conectado a base de datos");
+	else {
+    console.log("Conectado a base de datos");
+    connection.query("select (SELECT group_concat(DISTINCT TRADER) FROM legajos) as TRADER, (SELECT group_concat(DISTINCT PRODUCT_O) FROM legajos) as PRODUCT_O,(SELECT group_concat(DISTINCT PRODUCT_R) FROM legajos) as PRODUCT_R,(SELECT group_concat(DISTINCT TRADER) FROM legajos) as TRADER, (SELECT group_concat(DISTINCT ZONA) FROM legajos) as ZONA, (SELECT group_concat(DISTINCT TIPO_INMU) FROM legajos) as TIPO_INMU,(SELECT group_concat(DISTINCT PAIS) FROM legajos) as PAIS,(SELECT group_concat(DISTINCT ESTADO) FROM legajos) as ESTADO", function (err,result,fields){
+      if (err) throw err;
+      campos = result;
+    });
+  }
 });
+
+var campos = []
 
 app.get("/propiedades", function(req,res){
   connection.query("Select LEGAJO, NOMBRE_INM, OPERACION, OFER_REQUE, FECHA from legajos LIMIT 10", function (err,result,fields){
     if (err) throw err;
     res.send(result);
   });
-  
 });
+
+app.get("/api/fields", function(req,res){
+    res.send(campos);
+});
+
 app.get("/propiedades/:legajo", function(req,res){
   connection.query("Select LEGAJO, NOMBRE_INM, OPERACION, OFER_REQUE, FECHA from legajos WHERE LEGAJO='"+ req.params.legajo+"' LIMIT 10", function (err,result,fields){
     if (err) throw err;
     res.send(result);
   });
-  
 });
-app.get("/estadisticas", function(req,res){
-  connection.query("SELECT count(*) Total, sum(TIPO_INMU = 'CA') CA, sum(TIPO_INMU = 'CQ') CQ, sum(TIPO_INMU = 'CC') CC, sum(TIPO_INMU = 'TU') TU, sum(TIPO_INMU = 'OT') OT,sum(TIPO_INMU = 'DE') DE, sum(TIPO_INMU = 'TC') TC, sum(TIPO_INMU = 'VU') VU, sum(TIPO_INMU = 'OF') OF, sum(TIPO_INMU = 'FA') FA, sum(TIPO_INMU = 'LO') LO  FROM legajos;", function (err,result,fields){
+
+app.get("/api/estadisticas", function(req,res){
+  connection.query("select TIPO_INMU COD, COUNT(TIPO_INMU) TOT from legajos GROUP BY TIPO_INMU; select PRODUCT_O COD, COUNT(PRODUCT_O) TOT from legajos GROUP BY PRODUCT_O; select PRODUCT_R COD, COUNT(PRODUCT_R) TOT from legajos GROUP BY PRODUCT_R; select TRADER COD, COUNT(TRADER) TOT from legajos GROUP BY TRADER", function (err,result,fields){
     if (err) throw err;
-    resultobj = result[0];
-    datos = [[],[]];
-    for(var property in resultobj) {
-      if (property != "Total") {
-        datos[0].push(100*resultobj[property]/resultobj["Total"]);
-        datos[1].push(property);
-      }
+    
+    tipo_inmu = result [0];
+    productores_o = result[1];
+    productores_r = result[2];
+    traders = result[3];
+    var max_array_size = Math.max(productores_o.length, productores_r.length, traders.length);
+    var estadisticas = {
+      TIPO_INMU : [[],[]],
+      PRODUCTORES : [new Array(max_array_size),new Array(max_array_size),new Array(max_array_size),new Array(max_array_size)]
     }
-    res.send(datos);
+    var name = "";
+    for (var i in tipo_inmu) {
+      if (tipo_inmu[i].COD != ""){
+        estadisticas.TIPO_INMU[1].push(tipo_inmu[i].TOT)
+        estadisticas.TIPO_INMU[0].push(tipo_inmu[i].COD)
+      } 
+    }
+    for (var i in productores_o) {
+      if (productores_o[i].COD != ""){
+        name = productores_o[i].COD;
+      }
+      else {
+        name = "None"
+      }
+      estadisticas.PRODUCTORES[0][i] = name;
+      estadisticas.PRODUCTORES[1][i] = productores_o[i].TOT;
+    }
+    for (var i in productores_r) {
+      if (productores_r[i].COD != ""){
+        name = productores_r[i].COD;
+      }
+      else {
+        name = "None"
+      }
+      if(estadisticas.PRODUCTORES[0].indexOf(name)<0){
+        estadisticas.PRODUCTORES[0].push(name)
+        estadisticas.PRODUCTORES[1].push(0)
+      }
+      estadisticas.PRODUCTORES[2][estadisticas.PRODUCTORES[0].indexOf(name)] = productores_r[i].TOT;
+    }
+    for (var i in traders) {
+      if (traders[i].COD != ""){
+         name = traders[i].COD;
+      }
+      else {
+        name = "None";
+      }
+      if(estadisticas.PRODUCTORES[0].indexOf(name)<0){
+        estadisticas.PRODUCTORES[0].push(name)
+        estadisticas.PRODUCTORES[1].push(0)
+        estadisticas.PRODUCTORES[2].push(0)
+      }
+      estadisticas.PRODUCTORES[3][estadisticas.PRODUCTORES[0].indexOf(name)] = traders[i].TOT
+    }
+    res.send(estadisticas);
   });
 });
 
